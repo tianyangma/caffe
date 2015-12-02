a925fa696fb8cac53f55a3c52dfbeec1b83bc24c
Tianyang Ma <ma.tianyang@gmail.com>
2015-12-02 10:08:41 +0800

    Move the MultiBoxLossLayer definition to a separate header.

--- src/caffe/layers/multibox_loss_layer.cpp
+++ src/caffe/layers/multibox_loss_layer.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-#include "caffe/layer.hpp"
+#include "caffe/multibox_loss_layer.hpp"
 #include "caffe/util/io.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/vision_layers.hpp"
